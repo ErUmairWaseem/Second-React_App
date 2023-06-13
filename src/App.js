34f1c9357f528cb79ea1eb7f992f073f3f71eeb6
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-13 17:27:35 +0530

    inline style

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let str = `Hello Universe`;
-  let pi = 3.141159;
-  let user = {id:1, name: "Umair"};
+
+  let style = { color : "Red", background: "pink"};
 
   return(
     <>
-      <h1>{str}</h1>
-      <h1>PI: {pi}</h1>
-      <h1>
-        ID: {user.id}   Name: {user.name};
-      </h1>
+    <h1 style = {style}>
+      hello
+    </h1>
     </>
-  );
+  )
+
 }
 
 export default App;
