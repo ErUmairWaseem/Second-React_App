bac992b5dd3020779cf8cc365db191b13ef39098
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-13 16:52:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let str = "Hello World";
+  let str = `Hello Universe`;
+  let pi = 3.141159;
+  let active = true;
 
   return(
     <>
-      <h1>Good AfterNoon</h1>
       <h1>{str}</h1>
-      <h1>{str.toUpperCase()}</h1>
-      <h1>{str.toLocaleLowerCase()}</h1>
-      <h1>{str.substring(1,7)}</h1>
-   
+      <h1>PI = {pi} !!</h1>
+
+      <h1>{active ? "Good Morning" : "Good Morning"}</h1>
+
+      {active && <h1>Good Evening</h1>}
+      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
     </>
-    
   );
 }
 
