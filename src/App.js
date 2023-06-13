8d2cef6e323f700a9c9eb1c1eabc5570c64226cc
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-13 15:54:19 +0530

    hello universe

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-      excepturi ad. Enim qui distinctio cum explicabo molestiae dolorum, dolor
-      ut fugiat commodi maxime beatae accusamus.
-    </h1>
-  );
+  let l = <h1>Hello Universe</h1>;
+
+  return l;
 }
 
 export default App;
