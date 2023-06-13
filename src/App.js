626730f73daa0332662877d89d92cc610debb3cf
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-13 19:04:11 +0530

    b9 block

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 //import "./App.css";
+import DataBlock from "./DataBlock";
 
 function App() {
   return (
+
     <>
-      <h1 className="bg-primary text-light p-3">My Projects</h1>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit omnis
-        laboriosam dolor recusandae accusamus perspiciatis debitis dolore soluta
-        delectus qui maxime nam vel eos voluptate expedita magni, pariatur harum
-        eveniet asperiores necessitatibus minima veniam fuga! Fugiat totam
-        veniam deleniti pariatur! Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Odio nesciunt ipsa, ullam, explicabo enim dolore
-        libero commodi distinctio facilis temporibus vitae expedita, ad quo?
-        Autem a magni animi nihil quam necessitatibus enim fuga qui architecto,
-        commodi est aperiam, eligendi aliquid libero sed ducimus ullam. Itaque
-        dolorem tempora aperiam tempore quo.
-        
-      </div>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit omnis
-        laboriosam dolor recusandae accusamus perspiciatis debitis dolore soluta
-        delectus qui maxime nam vel eos voluptate expedita magni, pariatur harum
-        eveniet asperiores necessitatibus minima veniam fuga! Fugiat totam
-        veniam deleniti pariatur! Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Odio nesciunt ipsa, ullam, explicabo enim dolore
-        libero commodi distinctio facilis temporibus vitae expedita, ad quo?
-        Autem a magni animi nihil quam necessitatibus enim fuga qui architecto,
-        commodi est aperiam, eligendi aliquid libero sed ducimus ullam. Itaque
-        dolorem tempora aperiam tempore quo.
-        
-      </div>
-
-      <div className="alert alert-primary">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit omnis
-        laboriosam dolor recusandae accusamus perspiciatis debitis dolore soluta
-        delectus qui maxime nam vel eos voluptate expedita magni, pariatur harum
-        eveniet asperiores necessitatibus minima veniam fuga! Fugiat totam
-        veniam deleniti pariatur! Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Odio nesciunt ipsa, ullam, explicabo enim dolore
-        libero commodi distinctio facilis temporibus vitae expedita, ad quo?
-        Autem a magni animi nihil quam necessitatibus enim fuga qui architecto,
-        commodi est aperiam, eligendi aliquid libero sed ducimus ullam. Itaque
-        dolorem tempora aperiam tempore quo.
-        
-      </div>
-    </>
+    <h1 className="bg-primary text-light p-3" >My Project</h1>
+    <DataBlock />
+    <DataBlock />
+    <DataBlock />
+  </>
   );
+ 
 }
 
 export default App;
---
+++ src/DataBlock.js
@@ -0,0 +1,17 @@
+
+
+function DataBlock() {
+  return (
+    <>
+      <div className="alert alert-primary" >
+        <h1>Lorem ipsum dolor sit amet. </h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          cupiditate illum error harum voluptatibus soluta neque inventore ex
+          excepturi, non laboriosam deleniti pariatur consequatur consequuntur!
+        
+      </div>
+    </>
+  );
+}
+
+export default DataBlock;
