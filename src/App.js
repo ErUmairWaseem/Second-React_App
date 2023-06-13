1e024e83ce2e0f67bb4750696aeb4cf9ea432329
UMAIR WASEEM KAHN <umairwaseem57@gmail.com>
2023-06-13 18:08:30 +0530

    b11-done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 //import "./App.css";
 
-
 function App() {
-
-  return(
+  return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="bg-primary text-light p-3">My Projects</h1>
+
+      <div className="alert alert-primary">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit omnis
+        laboriosam dolor recusandae accusamus perspiciatis debitis dolore soluta
+        delectus qui maxime nam vel eos voluptate expedita magni, pariatur harum
+        eveniet asperiores necessitatibus minima veniam fuga! Fugiat totam
+        veniam deleniti pariatur! Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Odio nesciunt ipsa, ullam, explicabo enim dolore
+        libero commodi distinctio facilis temporibus vitae expedita, ad quo?
+        Autem a magni animi nihil quam necessitatibus enim fuga qui architecto,
+        commodi est aperiam, eligendi aliquid libero sed ducimus ullam. Itaque
+        dolorem tempora aperiam tempore quo.
+      </div>
     </>
-
   );
-
 }
 
 export default App;
